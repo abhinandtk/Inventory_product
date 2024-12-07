431679d7cbadc0be8dc18ae0a60f040a3d20f0b0
Abhinand <abhinandtk69@gmail.com>
2024-12-07 07:20:44 +0530

    first commit

---
+++ product_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+import uuid
+from versatileimagefield.fields import VersatileImageField
+
+# Create your models here.
+
+class Products(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    ProductID=models.BigIntegerField(unique=True)
+    ProductCode=models.CharField(max_length=225,unique=True)
+    ProductName=models.CharField(max_length=225)
+    ProductImage=VersatileImageField(upload_to="uploads/",blank=True,null=True)
+    CreatedDate=models.DateTimeField(auto_now_add=True)
+    UpdatedDate=models.DateTimeField(blank=True,null=True)
+    CreatedUser=models.ForeignKey("auth.User",related_name="user%(class)s_objects",on_delete=models.CASCADE)
+    IsFavourite=models.BooleanField(default=False)
+    Active=models.BooleanField(default=True)
+    HSNCode=models.CharField(max_length=225,blank=True,null=True)
+    TotalStock=models.DecimalField(default=0.00,max_digits=20,decimal_places=8,blank=True,null=True)
+     
+    class Meta:
+        db_table="products_product"
+        verbose_name="product"
+        verbose_name_plural = "products"
+        unique_together = (("ProductCode", "ProductID"),)
+        ordering = ("-CreatedDate", "ProductID")
